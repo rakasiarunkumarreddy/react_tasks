695f78f96d650c8e69e4542e0f47cbcd5dd97974
ArunReddy <rakasiarunkumarreddy@gmail.com>
2024-12-04 15:06:29 +0530

    task 7 added

--- src/propdrilling/parentCompo.js
+++ src/propdrilling/parentCompo.js
@@ -6,7 +6,6 @@ const Parent = () => {
   return (
     <div>
       <Intermediate data="passing data from parent to nested child named Component named end.js"/>
-      <h1>Task 7 Completed</h1>
       <hr style={{ border: "none", borderTop: "2px dotted #333", margin: "20px 0" }} />
     </div>
   )
